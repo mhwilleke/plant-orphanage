c9886dfbd4a404f6a7fd91cccc11ff06b9541190
mhwilleke <mhwilleke@gmail.com>
2023-04-08 20:50:18 -0400

    B - Lint now passes. Fixed some and removed one rule that I didn't want anyway.

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,6 @@ import 'lazysizes';
 // global alert
 import './assets/js/alert';
 import { createClient } from '@supabase/supabase-js'
-import { data } from 'autoprefixer';
 
 // Create a single supabase client for interacting with your database 
 const supabase = createClient('https://lbnctgyadxhjbualvhbi.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxibmN0Z3lhZHhoamJ1YWx2aGJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDgzOTQyMjQsImV4cCI6MTk2Mzk3MDIyNH0.EXrx5wjh0w8_vBGUPfNR-PWpKU_MxjIqzR9Y2Miozsk')
@@ -26,12 +25,12 @@ async function submit_adoption_form(e) {
 function parseTheForm() {
     var plantswanted = document.querySelectorAll(".number-wanted");
     let data = {
-        adopter:{},
-        plants:[]
+        adopter: {},
+        plants: [],
     };
     for (const orphan of plantswanted) {
         if (orphan.value != 0) {
-            data.plants.push({plant:orphan.dataset.plant, number:parseInt(orphan.value)});
+            data.plants.push({ plant: orphan.dataset.plant, number: parseInt(orphan.value) });
         }
     }
     var adopter = document.querySelector("#adopter");
@@ -45,21 +44,21 @@ function parseTheForm() {
 async function sendToServer(data) {
     console.log("sending adoption request", data);
     await createAdopter(data.adopter);
-    for (const orphan of data.plants){
+    for (const orphan of data.plants) {
         await adoptPlant(orphan, data.adopter);
     }
-    thankyou(data.plants);
+    thankyou();
 }
-function thankyou(plants){
+function thankyou() {
     var adoption_form = document.querySelector("#adoption-form");
     const message = document.createElement("p");
-    message.textContent = "Your request has been recorded! Marian (marianhhartman@gmail.com) will be in contact with you soon."; 
+    message.textContent = "Your request has been recorded! Marian (marianhhartman@gmail.com) will be in contact with you soon.";
     adoption_form.parentElement.append(message);
     adoption_form.remove();
 }
 async function createAdopter(person) {
     console.log("creating adopter", person);
-    const { data:response, error } = await supabase
+    const { data: response, error } = await supabase
         .from('Adopters')
         .insert([
             { email: person.email, name: person.name, address: person.address },
@@ -68,23 +67,23 @@ async function createAdopter(person) {
 }
 async function adoptPlant(plant, person) {
     console.log(person, "is adopting", plant);
-    let { data:response, error } = await supabase
+    let { data: response, error } = await supabase
         .from('AdoptedPlants')
         .insert([
             { plant_type: plant.plant, inventory_requested: plant.number, requester: person.email },
         ]);
     console.log(response, error);
-    ({ data:response, error } = await supabase
+    ({ data: response, error } = await supabase
         .rpc('reduce-inventory', {
-            number_adopted:plant.number, 
-            plant:plant.plant
-          }));
+            number_adopted: plant.number,
+            plant: plant.plant,
+        }));
     console.log(response, error);
 }
 async function prepare_adoption_form() {
-    let { data: plants, error } = await supabase
-    .from('OrphanedPlants')
-    .select('plant_type,inventory_remaining,Plant_info,availability').order("plant_type");
+    let { data: plants } = await supabase
+        .from('OrphanedPlants')
+        .select('plant_type,inventory_remaining,Plant_info,availability').order("plant_type");
     var placeholder = document.querySelector("#placeholder");
     var template = document.querySelector('#orphaned-plant');
     for (const orphan of plants) {
@@ -93,36 +92,36 @@ async function prepare_adoption_form() {
         link.textContent = orphan.plant_type;
         link.href = orphan.Plant_info;
         var availability = clone.querySelector("#availability-placeholder")
-        if(orphan.inventory_remaining === 0){
+        if (orphan.inventory_remaining === 0) {
             availability.remove();
         }
         availability.textContent = "| available " + orphan.availability
 
         var dropdown = clone.querySelector("#adopting-number");
 
-        if(orphan.inventory_remaining === 0) {
+        if (orphan.inventory_remaining === 0) {
             var message = document.createElement("p");
             message.textContent = "All out!";
             message.style = "padding-left: 1em;";
             dropdown.after(message);
             dropdown.remove();
         } else {
-            for(var i = 1; i <= orphan.inventory_remaining; ++i) {
-                var option = document.createElement("option"); 
-                option.textContent = i; 
+            for (var i = 1; i <= orphan.inventory_remaining; ++i) {
+                var option = document.createElement("option");
+                option.textContent = i;
                 option.value = i;
                 dropdown.appendChild(option);
             }
             dropdown.dataset.plant = orphan.plant_type;
         }
-        
+
         placeholder.appendChild(clone);
     }
     var button = document.querySelector("#adoption-form")
     button.addEventListener('submit', submit_adoption_form, false);
 }
 
-if(document.readyState === "interactive" || document.readyState === "complete" || document.readyState === "loaded")
+if (document.readyState === "interactive" || document.readyState === "complete" || document.readyState === "loaded")
     prepare_adoption_form();
 else {
     document.addEventListener('DOMContentLoaded', prepare_adoption_form, false);
