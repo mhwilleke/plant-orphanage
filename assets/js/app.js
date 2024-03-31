d6229fa17276bf33a0efe27e7c47ba68cac36a61
Marian <marianhhartman@gmail.com>
2024-03-31 17:05:29 -0400

    2024 season commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -83,7 +83,7 @@ async function adoptPlant(plant, person) {
 async function prepare_adoption_form() {
     let { data: plants } = await supabase
         .from('OrphanedPlants')
-        .select('plant_type,inventory_remaining,Plant_info,availability').order("plant_type");
+        .select('plant_type,inventory_remaining,Plant_info,notes').order("plant_type");
     var placeholder = document.querySelector("#placeholder");
     var template = document.querySelector('#orphaned-plant');
     for (const orphan of plants) {
@@ -91,11 +91,11 @@ async function prepare_adoption_form() {
         var link = clone.querySelector("#plant-name");
         link.textContent = orphan.plant_type;
         link.href = orphan.Plant_info;
-        var availability = clone.querySelector("#availability-placeholder")
+        var notes = clone.querySelector("#notes-placeholder")
         if (orphan.inventory_remaining === 0) {
-            availability.remove();
+            notes.remove();
         }
-        availability.textContent = "| available " + orphan.availability
+        notes.textContent = orphan.notes
 
         var dropdown = clone.querySelector("#adopting-number");
 
--- content/_index.md
+++ content/_index.md
@@ -1,7 +1,7 @@
 ---
 title : "Plant Orphanage"
 description: "Hugo starter helping you build modern websites."
-lead: "Adopt your free greenhouse plants!"
+lead: "Greenhouse Plants Ready for Adoption!"
 date: 2020-04-17T12:18:10+00:00
 lastmod: 2020-04-17T12:18:10+00:00
 draft: false
