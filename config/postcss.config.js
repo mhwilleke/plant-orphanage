e05c18bec9b109c794a3692afe440ab9d5474573
Arlo Belshee <arlo@belshee.com>
2025-03-28 10:42:16 -0400

    Stop purging "unnecessary" CSS, as it was getting everything. We don't care much about download size, so make it simple.

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,35 +1,7 @@
 import autoprefixer from 'autoprefixer';
-import whitelister from 'purgecss-whitelister';
-import { purgeCSSPlugin } from "@fullhuman/postcss-purgecss";
-
-const purgecss = purgeCSSPlugin({
-  content: ["./hugo_stats.json"],
-  defaultExtractor: (content) => {
-    const els = JSON.parse(content).htmlElements;
-    return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
-  },
-  safelist: [
-    "lazyloaded",
-    "table",
-    "thead",
-    "tbody",
-    "tr",
-    "th",
-    "td",
-    "h3",
-    "alert-link",
-    ...whitelister([
-      "./assets/scss/components/_alerts.scss",
-      // './assets/scss/components/_buttons.scss',
-      "./assets/scss/components/_code.scss",
-      // './assets/scss/components/_syntax.scss',
-    ]),
-  ],
-});
 
 export default {
   plugins: [
     autoprefixer(),
-    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
   ],
 };
