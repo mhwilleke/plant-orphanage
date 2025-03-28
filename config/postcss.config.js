d6c70cfeb5afd575d36ace8d350a5324b694b875
Arlo Belshee <arlo@belshee.com>
2025-03-28 10:36:41 -0400

    Update postcss config & purgecss to work with current versions.

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,32 +1,35 @@
-const autoprefixer = require('autoprefixer');
-const purgecss = require('@fullhuman/postcss-purgecss');
-const whitelister = require('purgecss-whitelister');
+import autoprefixer from 'autoprefixer';
+import whitelister from 'purgecss-whitelister';
+import { purgeCSSPlugin } from "@fullhuman/postcss-purgecss";
 
-module.exports = {
+const purgecss = purgeCSSPlugin({
+  content: ["./hugo_stats.json"],
+  defaultExtractor: (content) => {
+    const els = JSON.parse(content).htmlElements;
+    return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
+  },
+  safelist: [
+    "lazyloaded",
+    "table",
+    "thead",
+    "tbody",
+    "tr",
+    "th",
+    "td",
+    "h3",
+    "alert-link",
+    ...whitelister([
+      "./assets/scss/components/_alerts.scss",
+      // './assets/scss/components/_buttons.scss',
+      "./assets/scss/components/_code.scss",
+      // './assets/scss/components/_syntax.scss',
+    ]),
+  ],
+});
+
+export default {
   plugins: [
     autoprefixer(),
-    purgecss({
-      content: [
-        './layouts/**/*.html',
-        './content/**/*.md',
-      ],
-      safelist: [
-        'lazyloaded',
-        'table',
-        'thead',
-        'tbody',
-        'tr',
-        'th',
-        'td',
-        'h3',
-        'alert-link',
-        ...whitelister([
-          './assets/scss/components/_alerts.scss',
-          // './assets/scss/components/_buttons.scss',
-          './assets/scss/components/_code.scss',
-          // './assets/scss/components/_syntax.scss',
-        ]),
-      ],
-    }),
+    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
   ],
-}
+};
